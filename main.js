4e82f2967136e502b020f0273c6d6d3eaeff3c7f
hayoung <ha02e@naver.com>
2024-02-20 17:12:43 +0900

    누나 api로 수정(newsapi 단점 때문에)

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const API_KEY = `fee0da4c686f44969b929bbc84192c2a`;
+let news = [];
 
 //뉴스 불러오는 함수
 const getLatestNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    `https://hy-news-times.netlify.app/?country=kr&apiKey=${API_KEY}`
   );
   //url 호출
   const response = await fetch(url);
   const data = await response.json(); //json:파일형식
-  console.log("dddd", data.articles);
+  news = data.articles;
+  console.log("dddd", news);
 };
 
 getLatestNews();
