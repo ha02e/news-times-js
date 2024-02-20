2c09da8352d7d3f70501fd9c8376ab0bf45f101e
hayoung <ha02e@naver.com>
2024-02-20 17:23:25 +0900

    누나 api 수정

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ let news = [];
 //뉴스 불러오는 함수
 const getLatestNews = async () => {
   const url = new URL(
-    `https://hy-news-times.netlify.app/?country=kr&apiKey=${API_KEY}`
+    `https://hy-news-times.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
   );
   //url 호출
   const response = await fetch(url);
   const data = await response.json(); //json:파일형식
   news = data.articles;
-  console.log("dddd", news);
+  //console.log("dddd", news);
 };
 
 getLatestNews();
