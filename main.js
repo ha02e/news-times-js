12ee258d2732d0969227bd21bb3aa2320ea3fb86
hayoung <ha02e@naver.com>
2024-02-20 17:03:55 +0900

    api 데이터 받아오기

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const API_KEY = `fee0da4c686f44969b929bbc84192c2a`;
 
 //뉴스 불러오는 함수
-const getLatestNews = () => {
+const getLatestNews = async () => {
   const url = new URL(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   );
   //url 호출
-  const response = fetch(url);
+  const response = await fetch(url);
+  const data = await response.json(); //json:파일형식
+  console.log("dddd", data.articles);
 };
 
 getLatestNews();
