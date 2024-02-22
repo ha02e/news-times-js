f4863d4008f30000d70dd9787d17edf112ded04c
hayoung <ha02e@naver.com>
2024-02-22 18:18:38 +0900

    메뉴에 따라 뉴스 보여주기 완료

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
 const API_KEY = `fee0da4c686f44969b929bbc84192c2a`;
 let newsList = [];
+let searchArea = document.getElementById("search-area");
 let searchIcon = document.getElementById("search-icon");
+const menus = document.querySelectorAll(".menus button");
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+
+//검색
+const openSearchArea = () => {
+  if (searchArea.style.display === "none") {
+    searchArea.style.display = "flex";
+    searchIcon.style.display = "none";
+  } else {
+    searchArea.style.display = "none";
+    searchIcon.style.display = "block";
+  }
+};
+
+const searchNews = () => {
+  searchIcon.style.display = "block";
+  searchArea.style.display = "none";
+};
+
+// 모바일 슬라이드 메뉴
+const openNav = () => {
+  document.getElementById("sideNav").style.width = "70vw";
+};
+const closeNav = () => {
+  document.getElementById("sideNav").style.width = "0";
+};
 
 //뉴스 불러오는 함수
 const getLatestNews = async () => {
   const url = new URL(
-    `https://hy-news-times.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}&pageSize=13`
   );
-  //https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}&pageSize=13
+  //https://newsapi.org/v2/top-headlines?country=kr&pageSize=13&apiKey=${API_KEY}
   //https://hy-news-times.netlify.app/top-headlines?country=us&apiKey=${API_KEY}
 
   //url 호출
@@ -16,6 +45,20 @@ const getLatestNews = async () => {
   newsList = data.articles;
   render();
   console.log("dddd", newsList);
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log(category);
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
+  );
+
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  console.log("DDDD", data);
+  render();
 };
 
 const render = () => {
@@ -56,32 +99,6 @@ const render = () => {
 
 getLatestNews();
 
-//검색
-const openSearchArea = () => {
-  let searchArea = document.getElementById("search-area");
-  let searchIcon = document.getElementById("search-icon");
-
-  if (searchArea.style.display === "none") {
-    searchArea.style.display = "flex";
-    searchIcon.style.display = "none";
-  } else {
-    searchArea.style.display = "none";
-    searchIcon.style.display = "block";
-  }
-};
-
-const searchNews = () => {
-  let searchArea = document.getElementById("search-area");
-  let searchIcon = document.getElementById("search-icon");
-
-  searchIcon.style.display = "block";
-  searchArea.style.display = "none";
-};
-
-// 모바일 슬라이드 메뉴
-const openNav = () => {
-  document.getElementById("sideNav").style.width = "70vw";
-};
-const closeNav = () => {
-  document.getElementById("sideNav").style.width = "0";
-};
+//1.버튼들에 클릭 이벤트 추가
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
